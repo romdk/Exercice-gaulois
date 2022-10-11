3bed9950a3c07dc405acee6a4c6ebc65c74df58c
romdk <drack.roman67@gmail.com>
2022-10-11 14:09:27 +0200

    requete

--- exercice1.sql
+++ exercice1.sql
@@ -1,6 +1,6 @@
 Nombre de gaulois par lieu (trié par nombre de gaulois décroissant)
 
-SELECT count(*) 
+SELECT COUNT(*),id_lieu
 FROM personnage
 GROUP BY id_lieu
-ORDER BY count(*)
+ORDER BY count(*)DESC
